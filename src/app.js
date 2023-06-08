b01b37efdaa745f2da580c838850ce35e44d0df1
bertablancpastor <72865106+bertablancpastor@users.noreply.github.com>
2023-06-08 09:43:31 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,71 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+const palos = ["♦", "♥", "♠", "♣"];
+let figura = palos[randomNum(palos)];
+
+const num = document.querySelector("#number");
+const fig1 = document.querySelectorAll("#palo")[0];
+const fig2 = document.querySelectorAll("#palo")[1];
+
+const button = document.querySelector("#newCard");
+
+function randomNum(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  if (!fig1.classList.contains("red") || !fig1.classList.contains("black")) {
+    if (figura === "♦" || figura === "♥") {
+      fig1.classList.add("red");
+      fig2.classList.add("red");
+      num.classList.add("red");
+    } else {
+      fig1.classList.add("black");
+      fig2.classList.add("black");
+      num.classList.add("black");
+    }
+  }
+
+  num.innerHTML = numbers[randomNum(numbers)];
+  fig1.innerHTML = figura;
+  fig2.innerHTML = figura;
 };
+
+function onClick() {
+  num.innerHTML = numbers[randomNum(numbers)];
+  fig1.innerHTML = palos[randomNum(palos)];
+  fig2.innerHTML = fig1.innerHTML;
+
+  if (fig1.innerHTML === "♦" || fig1.innerHTML === "♥") {
+    if (fig1.classList.contains("red")) {
+      return;
+    }
+    fig1.classList.replace("black", "red");
+    fig2.classList.replace("black", "red");
+    num.classList.replace("black", "red");
+  } else {
+    fig1.classList.replace("red", "black");
+    fig2.classList.replace("red", "black");
+    num.classList.replace("red", "black");
+  }
+}
+
+button.addEventListener("click", onClick);
